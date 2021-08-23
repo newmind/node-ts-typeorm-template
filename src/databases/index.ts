294f2e52796bea8a68f034c4af6ded2a021854db
jg <jg@wired.company>
2021-08-23 14:33:08 +0900

    sqlite 개발 환경 추가, DB_TYPE 환경 변수 추가

--- src/databases/index.ts
+++ src/databases/index.ts
@@ -5,7 +5,22 @@ import { dbConfig } from '@interfaces/db.interface';
 
 const { host, user, password, database }: dbConfig = config.get('dbConfig');
 
-export const dbConnection: ConnectionOptions = {
+const sqliteOptions: ConnectionOptions = {
+  type: 'sqlite',
+  database: 'database.sqlite',
+  synchronize: true,
+  logging: process.env.NODE_ENV !== 'production',
+  entities: [path.join(__dirname, '../**/*.entity{.ts,.js}')],
+  migrations: [path.join(__dirname, '../**/*.migration{.ts,.js}')],
+  subscribers: [path.join(__dirname, '../**/*.subscriber{.ts,.js}')],
+  cli: {
+    entitiesDir: 'src/entity',
+    migrationsDir: 'src/migration',
+    subscribersDir: 'src/subscriber',
+  },
+};
+
+const mysqlOptions: ConnectionOptions = {
   type: 'mysql',
   host: host,
   port: 3306,
@@ -23,3 +38,10 @@ export const dbConnection: ConnectionOptions = {
     subscribersDir: 'src/subscriber',
   },
 };
+
+let connectionOptions: ConnectionOptions = mysqlOptions;
+if (process.env.DB_TYPE == 'sqlite') {
+  connectionOptions = sqliteOptions;
+}
+
+export const dbConnection: ConnectionOptions = connectionOptions;
